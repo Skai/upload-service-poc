c79d5a44185a976a28ff01596989b77f1b3b71cf
Julie Ng <uxjulie@gmail.com>
2017-02-11 12:58:31 +0100

    Frontend: fix asset path for webpack in dev

--- client/vuejs/build/webpack.watch.conf.js
+++ client/vuejs/build/webpack.watch.conf.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack')
+var config = require('../config')
 var merge = require('webpack-merge')
 var baseConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
